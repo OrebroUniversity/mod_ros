051a51e99e997e33b623cd2fd629f292d2a1ca9f
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2018-08-03 16:25:25 +0200

    WIP - cliffmap_server

--- src/test_xml.cpp
+++ src/test_xml.cpp
@@ -1,14 +1,12 @@
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-
 #include <iostream>
-
 #include <cliffmap_ros/cliffmap.hpp>
+
+
 
 int main(int argn, char *argv[]) {
 
   if (argn < 2) {
-    std::cout << "usage test_xml <file_name>" << std::endl;
+    std::cout << "Usage cliffmap_server <file_name>" << std::endl;
     return -1;
   }
 
@@ -16,6 +14,6 @@ int main(int argn, char *argv[]) {
   std::cout << map;
 
   map.organizeAsGrid();
-  std::cout << map(12,12);
+  
   return 0;
 }
