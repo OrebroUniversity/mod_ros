5473010181c937f69315ce59ae97061c9b55aedf
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2018-08-06 15:01:00 +0200

    ROS service is ready.

--- src/cliffmap_server.cpp
+++ src/cliffmap_server.cpp
@@ -1,16 +1,44 @@
-#include <iostream>
+#include <ros/ros.h>
+
+#include <cliffmap_ros/CLiFFMapMsg.h>
+#include <cliffmap_ros/GetCLiFFMap.h>
+
 #include <cliffmap_ros/cliffmap.hpp>
+#include <iostream>
+
+cliffmap_ros::CLiFFMapMsg cliffmap;
+
+bool callback(cliffmap_ros::GetCLiFFMap::Request &req,
+              cliffmap_ros::GetCLiFFMap::Response &res) {
+  res.cliffmap = cliffmap;
+  return true;
+}
 
 int main(int argn, char *argv[]) {
+
+  ros::init(argn, argv, "cliffmap_server");
 
   if (argn < 2) {
     std::cout << "Usage cliffmap_server <file_name>" << std::endl;
     return -1;
   }
 
-  cliffmap_ros::CLiFFMap map(argv[1]);
-  std::cout << map;
+  ros::NodeHandle nh;
+  ros::Publisher cliffmap_pub =
+      nh.advertise<cliffmap_ros::CLiFFMapMsg>("cliffmap", 10);
+  ROS_INFO("CLiFFMap will be published when there is a subscriber.");
 
+  ros::ServiceServer service_ = nh.advertiseService("get_cliffmap", callback);
+
+  cliffmap_ros::CLiFFMap map(argv[1]);
   map.organizeAsGrid();
+
+  cliffmap = mapToROSMsg(map);
+
+  while (ros::ok()) {
+    if (cliffmap_pub.getNumSubscribers() > 0)
+      cliffmap_pub.publish(cliffmap);
+    ros::spinOnce();
+  }
   return 0;
 }
