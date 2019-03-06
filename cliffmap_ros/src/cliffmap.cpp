d95dee2bb179173e486fbed20af8148efeaf35a0
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2019-03-06 14:25:31 +0100

    Modify licence in sources

--- cliffmap_ros/src/cliffmap.cpp
+++ cliffmap_ros/src/cliffmap.cpp
@@ -1,3 +1,22 @@
+/*
+ *   Copyright (c) Chittaranjan Srinivas Swaminathan
+ *   This file is part of cliffmap_ros.
+ *
+ *   cliffmap_ros is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the License,
+ *   or (at your option) any later version.
+ *
+ *   cliffmap_ros is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with cliffmap_rviz_plugin.  If not, see
+ *   <https://www.gnu.org/licenses/>.
+ */
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <cliffmap_ros/cliffmap.hpp>
