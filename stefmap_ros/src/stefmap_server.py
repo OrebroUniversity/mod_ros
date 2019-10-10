65f810d81f595a15c9e17a82133fdf04f0c36e4a
sergimolina <sergimolina91@gmail.com>
2019-10-10 14:49:02 +0100

    changes to use actionlib to retrive the probabilities from fremenarray instead of using the topic publishing

--- stefmap_ros/src/stefmap_server.py
+++ stefmap_ros/src/stefmap_server.py
@@ -4,52 +4,44 @@ from stefmap_ros.msg import STeFMapMsg,STeFMapCellMsg
 from stefmap_ros.srv import GetSTeFMap
 import rospy
 import numpy as np
-from fremenarray.msg import FremenArrayActionGoal,FremenArrayActionResult
+from fremenarray.msg import FremenArrayActionGoal,FremenArrayActionResult, FremenArrayAction, FremenArrayGoal
 from os.path import expanduser
 import time as t
+import actionlib
 
 predicted_probabilities = []
 stefmap_pub = None
-
-
-def result_fremen_callback(data):
-	global predicted_probabilities
-	if len(data.result.probabilities)>0:
-		for i in range(0,len(data.result.probabilities)):
-			predicted_probabilities.append(data.result.probabilities[i])
-	print("Values received.")
-
-def predict_map(timestamp,order):
-	print("Making prediction...")
-	pub = rospy.Publisher('/fremenarray/goal', FremenArrayActionGoal, queue_size=1)
-	t.sleep(1)
-	frem_msg=FremenArrayActionGoal()
-	frem_msg.goal.operation = 'predict'
-	frem_msg.goal.order = int(order)
-	frem_msg.goal.time = timestamp
-	pub.publish(frem_msg)
-	t.sleep(1)
-
 
 def handle_GetSTeFMap(req):
 	global predicted_probabilities
 	global stefmap_pub
 
-	predict_map(req.header.stamp.to_sec(),req.order)
-
 	rows = int((req.y_max - req.y_min)/req.cell_size)
 	columns = int((req.x_max - req.x_min)/req.cell_size)
+
+	fremenarray_client = actionlib.SimpleActionClient('/fremenarray', FremenArrayAction)
 	
-	prob_matrix = np.zeros((rows,columns,8))
+	rospy.Duration(3.0)
+
+	if not fremenarray_client.wait_for_server(rospy.Duration(5.0)):
+		print "Cannot connect to FremenArray"
+		mSTefMap = STeFMapMsg()
+		return mSTefMap
+	
+	frem_msg=FremenArrayGoal()
+	frem_msg.operation = 'predict'
+	frem_msg.order = req.order
+	frem_msg.time = req.header.stamp.to_sec()
+
+	fremenarray_client.send_goal(frem_msg)
+	if not fremenarray_client.wait_for_result(rospy.Duration(10.0)):
+		print "Error getting the STeF-Map"
+		mSTefMap = STeFMapMsg()
+		return mSTefMap
+	fremenarray_result = fremenarray_client.get_result()
 
 
-	while len(predicted_probabilities) == 0:
-		#print len(predicted_probabilities)
-		rospy.logwarn_throttle(60, "[STeFMapServer]: Waiting for FreMEn services...")
-		t.sleep(1) 
-
-	prob_matrix[:,:,:] = np.reshape(predicted_probabilities,(rows,columns,8))
-	predicted_probabilities = []
+	prob_matrix = np.reshape(fremenarray_result.probabilities,(rows,columns,8))
 
 	# Creating the ros msg to has to be returned to client calling
 	mSTefMap = STeFMapMsg()
@@ -103,7 +95,6 @@ def handle_GetSTeFMap(req):
 if __name__ == "__main__":
     
 	rospy.init_node('get_stefmap_server')
-	rospy.Subscriber('/fremenarray/result', FremenArrayActionResult, result_fremen_callback)
 	stefmap_service = rospy.Service('get_stefmap', GetSTeFMap, handle_GetSTeFMap)
 	
 	stefmap_pub = rospy.Publisher('/stefmap', STeFMapMsg, queue_size=1, latch=True)
